23db6f393356331393346e4c0ec208e6015cf589
vikafilippok <dbrekzvbkfirf@icloud.com>
2024-03-02 19:59:57 +0300

    added arguments

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello, world!")
+import argparse
+import os
+import shutil
+import winreg
+
+
+command_line = argparse.ArgumentParser(prog="FILE SYSTEM|REGISTRY MANAGEMENT BY CLI")
+command_line.add_argument("--create", nargs=1, metavar="FILE", help="Create a new file", type=str)
+command_line.add_argument("--remove", metavar="FILE", help="Remove a file", nargs=1, type=str)
+command_line.add_argument("--write", metavar=("FILE", "TEXT"), help="Write to a file", nargs=2, type=str)
+command_line.add_argument("--read", metavar="FILE", help="Read from a file", nargs=1, type=str)
+command_line.add_argument("--copy", metavar=("FILE", "PATH1", "PATH2"), help="Copy FILE from PATH1 to PATH2 (folder without file name)", nargs=3, type=str)
+command_line.add_argument("--rename", metavar=("OLD", "NEW"), help="Rename a file", nargs=2, type=str)
+command_line.add_argument("--create-key", metavar="KEY", help="Create a new key", nargs=1, type=str)
+command_line.add_argument("--remove-key", metavar="KEY", help="Remove a key", nargs=1, type=str)
+command_line.add_argument("--write-key", metavar=("KEY", "NAME", "VALUE"), help="Write value to key", nargs=3, type=str)
+
+args = command_line.parse_args()
+
+
