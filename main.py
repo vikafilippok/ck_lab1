8ff29a535e84b9b8f1c8d795ba49d44c3798d8c9
vikafilippok <dbrekzvbkfirf@icloud.com>
2024-03-03 15:06:57 +0300

    ALL DONE

--- main.py
+++ main.py
@@ -3,18 +3,29 @@ import os
 import shutil
 import winreg
 
+
+########## CREATE FILE #########################
 def create(name):
     try:
         with open(name, "x") as file:
             print("File created")
     except FileExistsError:
         print("Error: file is already exist")
+    except Exception as e:
+        print(f"An error occurred while creating the key: {e}")
+
+########## REMOVE #########################
 def remove(name):
     try:
         os.remove(name)
         print("File deleted")
     except FileNotFoundError:
         print("Error: file not found")
+    except Exception as e:
+        print(f"An error occurred while creating the key: {e}")
+
+
+########## WRITE #########################
 def write(name, value):
     try:
         with open(name, "w") as file:
@@ -22,24 +33,115 @@ def write(name, value):
             print("Written to file")
     except FileNotFoundError:
         print("Error: file not found")
+    except Exception as e:
+        print(f"An error occurred while creating the key: {e}")
 
+
+########## READ #########################
 def read(name):
     try:
         with open(name, "r") as file:
             print(file.read())
     except FileNotFoundError:
         print("Error: file not found")
+    except Exception as e:
+        print(f"An error occurred while creating the key: {e}")
+
+
 def copy(full_path, directory):
     try:
         shutil.copy2(full_path, directory)
         print("File copied to another directory")
     except FileNotFoundError:
         print("Error: file not found")
+    except Exception as e:
+        print(f"An error occurred while creating the key: {e}")
 
-#def rename(old_name, new_name):
-#def create_key(key, name):
-#def remove_key(key, name):
-#def write_to_key(key, name, value):
+
+########## RENAME #########################
+def rename(old_name, new_name):
+    try:
+        os.renames(old_name, new_name)
+        print("File|Directory renamed from", old_name, "to", new_name)
+    except FileNotFoundError:
+        print("Error: file not found")
+    except Exception as e:
+        print(f"An error occurred while creating the key: {e}")
+
+
+########## CREATE KEY #########################
+def create_key(Hkey, sub_key):
+    try:
+        if Hkey == "HKEY_CLASSES_ROOT":
+            key = winreg.CreateKeyEx(winreg.HKEY_CLASSES_ROOT, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_CURRENT_USER":
+            key = winreg.CreateKeyEx(winreg.HKEY_CURRENT_USER, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_LOCAL_MACHINE":
+            software = winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\", 0, winreg.KEY_ALL_ACCESS)
+            key = winreg.CreateKeyEx(software, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_USERS":
+            key = winreg.CreateKeyEx(winreg.HKEY_USERS, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_CURRENT_CONFIG":
+            key = winreg.CreateKeyEx(winreg.HKEY_CURRENT_CONFIG, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        print("The key has been created")
+    except PermissionError:
+        print("Access error. Administrator rights are required to create a registry key.")
+    except Exception as e:
+        print(f"An error occurred while creating the key: {e}")
+
+
+########## REMOVE KEY #########################
+def remove_key(Hkey, sub_key):
+    try:
+        if Hkey == "HKEY_CLASSES_ROOT":
+            key = winreg.OpenKeyEx(winreg.HKEY_CLASSES_ROOT, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_CURRENT_USER":
+            key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_LOCAL_MACHINE":
+            software = winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\", 0, winreg.KEY_ALL_ACCESS)
+            key = winreg.OpenKeyEx(software, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_USERS":
+            key = winreg.OpenKeyEx(winreg.HKEY_USERS, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        elif Hkey == "HKEY_CURRENT_CONFIG":
+            key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_CONFIG, sub_key, 0, winreg.KEY_ALL_ACCESS)
+    except FileNotFoundError:
+        print("The key was not found")
+        return
+    winreg.CloseKey(key)
+    if Hkey == "HKEY_CLASSES_ROOTT":
+        winreg.DeleteKey(winreg.HKEY_CLASSES_ROOT, sub_key)
+    if Hkey == "HKEY_CURRENT_USER":
+        winreg.DeleteKey(winreg.HKEY_CURRENT_USER, sub_key)
+    if Hkey == "HKEY_LOCAL_MACHINE":
+        winreg.DeleteKey(software, sub_key)
+    if Hkey == "HKEY_USERS":
+        winreg.DeleteKey(winreg.HKEY_USERS, sub_key)
+    if Hkey == "HKEY_CURRENT_CONFIG":
+        winreg.DeleteKey(winreg.HKEY_CURRENT_CONFIG, sub_key)
+    print("The key removed successfully")
+
+
+########## WRITE TO KEY #########################
+def write_value_to_key(Hkey, sub_key, name, value):
+    try:
+        if Hkey == "HKEY_CLASSES_ROOT":
+            key = winreg.OpenKeyEx(winreg.HKEY_CLASSES_ROOT, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        if Hkey == "HKEY_CURRENT_USER":
+            key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        if Hkey == "HKEY_LOCAL_MACHINE":
+            software = winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\\", 0, winreg.KEY_ALL_ACCESS)
+            key = winreg.OpenKeyEx(software, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        if Hkey == "HKEY_USERS":
+            key = winreg.OpenKeyEx(winreg.HKEY_USERS, sub_key, 0, winreg.KEY_ALL_ACCESS)
+        if Hkey == "HKEY_CURRENT_CONFIG":
+            key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_CONFIG, sub_key, 0, winreg.KEY_ALL_ACCESS)
+    except FileNotFoundError:
+        print("The key was not found")
+        return
+    winreg.SetValueEx(key, name, 0, winreg.REG_SZ, value)
+    use_value = winreg.QueryValueEx(key, name)
+    print("A value has been added to the key:", use_value)
+
 
 ########### PARSE #################
 def parse():
@@ -48,13 +150,15 @@ def parse():
     command_line.add_argument("--remove", metavar="FILE", help="Remove a file", nargs=1, type=str)
     command_line.add_argument("--write", metavar=("FILE", "TEXT"), help="Write to a file", nargs=2, type=str)
     command_line.add_argument("--read", metavar="FILE", help="Read from a file", type=str)
-    command_line.add_argument("--copy", metavar=("PATH1", "PATH2"), help="Copy file from PATH1 to PATH2", nargs=2, type=str)
+    command_line.add_argument("--copy", metavar=("PATH1", "PATH2"), help="Copy file from PATH1 to PATH2", nargs=2,
+                              type=str)
     command_line.add_argument("--rename", metavar=("OLD", "NEW"), help="Rename a file", nargs=2, type=str)
-    command_line.add_argument("--createKey", metavar=("KEY", "NAME"), help="Create a new key", nargs=2, type=str)
-    command_line.add_argument("--removeKey", metavar=("KEY", "NAME"), help="Remove a key", nargs=2, type=str)
-    command_line.add_argument("--writeToKey", metavar=("KEY", "NAME", "VALUE"), help="Write value to key", nargs=3, type=str)
+    command_line.add_argument("--createKey", metavar=("KEY", "SUB_KEY"), help="Create a new SUB_KEY in KEY", nargs=2, type=str)
+    command_line.add_argument("--removeKey", metavar=("KEY", "SUB_KEY"), help="Remove a key", nargs=2, type=str)
+    command_line.add_argument("--writeToKey", metavar=("KEY", "SUB_KEY", "NAME", "VALUE"),help="Write value to key", nargs=4, type=str)
 
     return command_line.parse_args()
+
 
 ############ MAIN ##################
 arg = parse()
@@ -76,5 +180,4 @@ elif arg.createKey:
 elif arg.removeKey:
     remove_key(arg.removeKey[0], arg.removeKey[1])
 elif arg.writeToKey:
-    write_to_key(arg.writeToKey[0], arg.writeToKey[1], arg.writeToKey[2])
-
+    write_value_to_key(arg.writeToKey[0], arg.writeToKey[1], arg.writeToKey[2], arg.writeToKey[3])
